b87537b5f54aafc25d9abc6aed615d262fe55d8c
ANIL <anilkumarbhardwarj.ar@gmail.com>
2024-08-22 18:40:26 +0530

    last modified code

--- Enviornment/DEV/provider.tf
+++ Enviornment/DEV/provider.tf
@@ -12,3 +12,19 @@ provider "azurerm" {
     
   }
 }
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.114.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
